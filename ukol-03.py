535182be81586e179d54b348a4dd7ef183369fd3
Togtokh Hanilec Daribazar <togimng@gmail.com>
2022-03-28 17:00:35 +0200

    ukol 3

--- ukol-03.py
+++ ukol-03.py
@@ -1,8 +1,8 @@
 """
 První funkce ověří délku telefonního čísla.
 Uvažuj, že telefonní číslo může být devítimístné nebo třináctimístné (pokud je na začátku předvolba "+420").
-Nemusíte kontrolovat, zda uživatel zadal skutečně číslo, či zda jsou tam i jiné znaky.
-To jsme v kurzu zatím neřešili. Pokud je číslo platné, funkce vrátí hodnotu True, jinak vrátí hodnotu False.
+Nemusíte kontrolovat, zda uživatel zadal skutečně číslo, či zda jsou tam i jiné znaky. To jsme v kurzu zatím neřešili.
+Pokud je číslo platné, funkce vrátí hodnotu True, jinak vrátí hodnotu False.
 """
 
 def phone_number (tel_cislo:str):
@@ -12,3 +12,10 @@ def phone_number (tel_cislo:str):
         return False
 
 print(phone_number("++"))
+
+tel_cislo = int(input("Zadej číslo, kam chceš zprávu zaslat: "))
+mark = get_mark(points)
+if mark == 5:
+  points = int(input("Zadej počet bodů v opravném pokusu: "))
+  mark = get_mark(points)
+print(f"Výsledná známka je {mark}.")
